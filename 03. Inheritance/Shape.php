70bd0608a6f962f1c29406b8935ef4cf17e7ad0a
tomasbaublys <tomas@forumcinemashe.lt>
2017-12-10 14:55:49 +0200

    Add Shape.php file to Inheritance topic

---
+++ 03. Inheritance/Shape.php
@@ -0,0 +1,59 @@
+<?php 
+
+/* Classes defined as abstract may not be instantiated */
+
+abstract class Shape {
+	
+	protected $color;
+
+	public function __construct($color = 'red')
+	{
+		$this->color = $color;
+	}
+
+	public function getColor()
+	{
+		return $this->color;
+	}
+
+	/* When inheriting from an abstract class, all methods marked abstract in the parent's class declaration must be defined by the child */
+
+	abstract protected function getArea();
+}
+
+class Square extends Shape {
+	
+	protected $length = 4;
+
+	public function getArea()
+	{
+		return pow($this->length, 2);
+	}
+}
+
+class Triangle extends Shape {
+	
+	protected $base = 4;
+	protected $height = 7;
+
+	public function getArea()
+	{
+		return .5 * $this->base * $this->height;
+	}
+}
+
+class Circle extends Shape {
+	
+	protected $radius = 5;
+
+	public function getArea()
+	{
+		return M_PI * pow($this->radius, 2);
+	}
+}
+
+
+$circle = new Circle(); // long way to initiate method
+echo $circle->getArea() . '</br>';
+
+echo (new Circle)->getArea(); // short way to initiate method
